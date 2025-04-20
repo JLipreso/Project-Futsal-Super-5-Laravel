0a5f37e18e706f9d13b13cb8c7a88d50228c4db6
DESKTOP-6QFCR0F\user <jasonlipreso@gmail.com>
2025-04-20 23:38:39 +0800

    OK

---
+++ app/Http/Controllers/admin/SignIn.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SignIn extends Controller
+{
+    public static function signin(Request $request) {
+        return [];
+    }
+}
